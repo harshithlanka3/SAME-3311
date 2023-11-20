004bee9e9c389ebe353e8d77a2564e78cbd90817
harshithlanka3 <harshithlanka03@gmail.com>
2023-11-20 01:10:21 -0500

    Fixed 'minor' issue

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Troll World!');
+  res.end('Hello World!');
 }).listen(8080);
