f10318eb3e934ddd71c8992dd6a2f1c8370a76a6
Paige Holland <pholland7@gatech.edu>
2024-01-18 12:28:22 -0500

    Added new Manage Account page as well as sidebar!

--- frontend/s_a_m_e/lib/admin.dart
+++ frontend/s_a_m_e/lib/admin.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:s_a_m_e/account.dart';
 import 'package:s_a_m_e/colors.dart';
 import 'package:s_a_m_e/add.dart';
 import 'package:s_a_m_e/symptomlist.dart';
@@ -12,58 +13,76 @@ class Admin extends StatelessWidget {
       appBar: AppBar(
         title: const Text('S.A.M.E'),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: const Text('Manage Account'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ManageAccountPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: Align(
-          alignment: Alignment.topCenter,
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 25),
-              const Text('Hello Admin User!', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                style: const ButtonStyle(
-                  foregroundColor: MaterialStatePropertyAll<Color>(white),
-                  backgroundColor: MaterialStatePropertyAll<Color>(navy),
-                ),
-                child: const Text('Add Symptom', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SymptomCreationPage()),
-                  );
-                },
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 25),
+            const Text('Hello Admin User!', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: const ButtonStyle(
+                foregroundColor: MaterialStatePropertyAll<Color>(white),
+                backgroundColor: MaterialStatePropertyAll<Color>(navy),
               ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                style: const ButtonStyle(
-                  foregroundColor: MaterialStatePropertyAll<Color>(white),
-                  backgroundColor: MaterialStatePropertyAll<Color>(navy),
-                ),
-                child: const Text('Symptoms List', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SymptomsListPage()),
-                  );
-                },
+              child: const Text('Add Symptom', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SymptomCreationPage()),
+                );
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: const ButtonStyle(
+                foregroundColor: MaterialStatePropertyAll<Color>(white),
+                backgroundColor: MaterialStatePropertyAll<Color>(navy),
               ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                style: const ButtonStyle(
-                  foregroundColor: MaterialStatePropertyAll<Color>(white),
-                  backgroundColor: MaterialStatePropertyAll<Color>(navy),
-                ),
-                child: const Text('Diagnosis List', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
-                onPressed: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => SymptomCreationPage()),
-                  // );
-                },
+              child: const Text('Symptoms List', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SymptomsListPage()),
+                );
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: const ButtonStyle(
+                foregroundColor: MaterialStatePropertyAll<Color>(white),
+                backgroundColor: MaterialStatePropertyAll<Color>(navy),
               ),
-            ])),
+              child: const Text('Diagnosis List', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //       builder: (context) => SymptomCreationPage()),
+                // );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
