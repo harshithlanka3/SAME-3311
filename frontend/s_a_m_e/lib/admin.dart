886af6d1de3d53dfc4ab17654897498ccf13b84e
Paige Holland <pholland7@gatech.edu>
2024-01-18 20:50:43 -0500

    Added sample button to be connected to sign-up page for app disclaimer pop-up

--- frontend/s_a_m_e/lib/admin.dart
+++ frontend/s_a_m_e/lib/admin.dart
@@ -3,9 +3,77 @@ import 'package:s_a_m_e/account.dart';
 import 'package:s_a_m_e/colors.dart';
 import 'package:s_a_m_e/add.dart';
 import 'package:s_a_m_e/symptomlist.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Admin extends StatelessWidget {
   const Admin({super.key});
+
+  void _showDisclaimerDialog(BuildContext context) {
+    bool checkboxValue = false;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return AlertDialog(
+              title: const Text('Disclaimer:'),
+              content: Column(
+                children: <Widget>[
+                  Text(disclaimer),
+                  Row(
+                    children: <Widget>[
+                      Checkbox(
+                        value: checkboxValue,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            checkboxValue = value!;
+                          });
+                        },
+                      ),
+                      const Text('I agree to the terms and conditions.'),
+                    ],
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Close'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (checkboxValue) {
+                      Navigator.of(context).pop(); 
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Admin(),
+                        ),
+                      );
+                    } else {
+                      Fluttertoast.showToast(
+                        msg: 'Please agree to the terms and conditions.',
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.CENTER,
+                        timeInSecForIosWeb: 1,
+                        backgroundColor: Colors.red,
+                        textColor: Colors.white,
+                        fontSize: 16.0,
+                      );
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +90,8 @@ class Admin extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => ManageAccountPage()),
+                    builder: (context) => ManageAccountPage(),
+                  ),
                 );
               },
             ),
@@ -46,7 +115,8 @@ class Admin extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const SymptomCreationPage()),
+                    builder: (context) => const SymptomCreationPage(),
+                  ),
                 );
               },
             ),
@@ -61,7 +131,8 @@ class Admin extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => const SymptomsListPage()),
+                    builder: (context) => const SymptomsListPage(),
+                  ),
                 );
               },
             ),
@@ -80,9 +151,23 @@ class Admin extends StatelessWidget {
                 // );
               },
             ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: const ButtonStyle(
+                foregroundColor: MaterialStatePropertyAll<Color>(white),
+                backgroundColor: MaterialStatePropertyAll<Color>(navy),
+              ),
+              child: const Text('Disclaimer Placeholder for Sign-up Button', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0)),
+              onPressed: () {
+                // Show the disclaimer dialog when this button is pressed
+                _showDisclaimerDialog(context);
+              },
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+String disclaimer = "This application is for general informational purposes only and is not intended to be a substitute for professional medical advice. The information provided should not be considered as medical advice, and we do not guarantee its accuracy.\nOur application is a guideline, and individual cases may vary.\n\nWe are not liable for any loss or damage arising from the use of this information. Consult with qualified healthcare professionals for advice tailored to your specific circumstances.\n\nBy using this application, you agree to these terms. S.A.M.E. is not responsible for errors or omissions. Use this application responsibly and in accordance with applicable laws.\n";
