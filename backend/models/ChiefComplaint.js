cae2e27205ebf540e5206547d3ca91495d20c319
Paige Holland <pholland7@gatech.edu>
2024-01-31 12:29:12 -0500

    Removed backend folders

--- backend/models/ChiefComplaint.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ChiefComplaintSchema = new mongoose.Schema({
-  name: String,
-  symptoms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Symptom' }]
-});
-
-module.exports = mongoose.model('ChiefComplaint', ChiefComplaintSchema);
--- backend/models/Diagnosis.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const DiagnosisSchema = new mongoose.Schema({
-  name: String,
-  symptoms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Symptom' }]
-});
-
-module.exports = mongoose.model('Diagnosis', DiagnosisSchema);
