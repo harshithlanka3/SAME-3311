40f07112f215d021dc6a1779ba337648eaf53011
Ramya Subramaniam <ramyasub@gatech.edu>
2024-02-04 12:51:52 -0500

    Added a button to view admin requests. Created a page that user can scroll through to see requests. Currently it grabs symptoms, but once admin database side is complete, it will grab the information from the right table.

---
+++ frontend/s_a_m_e/lib/adminrequest.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:s_a_m_e/colors.dart';
+import 'package:s_a_m_e/firebase_service.dart';
+import 'dart:convert';
+import 'package:s_a_m_e/profilepicture.dart';
+
+class AdminRequestPage extends StatefulWidget {
+  const AdminRequestPage({super.key});
+
+  @override
+  _AdminRequestPageState createState() => _AdminRequestPageState();
+}
+
+class _AdminRequestPageState extends State<AdminRequestPage> {
+  late Future<List<Symptom>> requests;
+
+  @override
+  void initState() {
+    super.initState();
+    requests = FirebaseService().getAllSymptoms();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Admin Requests", style: TextStyle(fontSize: 36.0)),
+        // ignore: prefer_const_constructors, prefer_const_literals_to_create_immutables
+        actions: [ProfilePicturePage()]
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: FutureBuilder<List<Symptom>>(
+          future: requests,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (snapshot.hasData) {
+              return Scrollbar(
+                trackVisibility: true,
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: <Widget>[
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: teal),
+                            color: boxinsides,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: ListTile(
+                            title: Text(snapshot.data![index].name, style: const TextStyle(fontWeight: FontWeight.bold)),
+                            // subtitle: const Text('Symptom Description'), 
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                      ],
+                    );
+                  },
+                ),
+              );
+            } else {
+              return const Center(child: Text('No admin requests'));
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
