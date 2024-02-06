981006bd3ac3ab42f9a01e13f1f1fa60008bf82f
hlanka3 <hlanka3@gatech.edu>
2024-02-05 22:23:18 -0500

    Added function that pulls users with active admin requests

--- frontend/s_a_m_e/lib/firebase_service.dart
+++ frontend/s_a_m_e/lib/firebase_service.dart
@@ -4,10 +4,12 @@ class Symptom {
   final String name;
   //final List<ChiefComplaint> chiefComplaints;
 
-  Symptom({required this.name, /*required this.chiefComplaints*/});
+  Symptom({
+    required this.name,
+    /*required this.chiefComplaints*/
+  });
 
-  Symptom.fromJson(Map<String, dynamic> json)
-      : name = json['name'];
+  Symptom.fromJson(Map<String, dynamic> json) : name = json['name'];
 }
 
 class ChiefComplaint {
@@ -15,24 +17,29 @@ class ChiefComplaint {
 
   ChiefComplaint({required this.name});
 
-  ChiefComplaint.fromJson(Map<String, dynamic> json)
-    : name = json['name'];
-
+  ChiefComplaint.fromJson(Map<String, dynamic> json) : name = json['name'];
 }
 
 class UserClass {
   final String email;
   String role;
+  bool? activeRequest;
+  String? requestReason;
 
-  UserClass({required this.email, required this.role});
+  UserClass(
+      {required this.email,
+      required this.role,
+      this.activeRequest,
+      this.requestReason});
 
   factory UserClass.fromJson(Map<String, dynamic> json) {
-  return UserClass(
-    email: json['email'],
-    role: json['role'] ?? 'user', 
-  );
-}
-
+    return UserClass(
+      email: json['email'],
+      role: json['role'] ?? 'user',
+      activeRequest: json['activeRequest'],
+      requestReason: json['requestReason'],
+    );
+  }
 }
 
 class FirebaseService {
@@ -40,21 +47,23 @@ class FirebaseService {
   final _chiefRef = FirebaseDatabase.instance.ref('data/chiefComplaints');
   final _usersRef = FirebaseDatabase.instance.ref('users/');
 
-  Future<int> addSymptom(String name, List<ChiefComplaint> chiefComplaints) async {
-  try {
-    DatabaseReference newSymptomRef = _symptomsRef.push();
+  Future<int> addSymptom(
+      String name, List<ChiefComplaint> chiefComplaints) async {
+    try {
+      DatabaseReference newSymptomRef = _symptomsRef.push();
 
-    List<String> complaintNames = chiefComplaints.map((complaint) => complaint.name).toList();
+      List<String> complaintNames =
+          chiefComplaints.map((complaint) => complaint.name).toList();
 
-    await newSymptomRef.set({'name': name, 'chiefComplaints': complaintNames});
-    print('Data added successfully');
-    return 200;
-  } catch (e) {
-    print('Error adding data: $e');
-    return 400;
+      await newSymptomRef
+          .set({'name': name, 'chiefComplaints': complaintNames});
+      print('Data added successfully');
+      return 200;
+    } catch (e) {
+      print('Error adding data: $e');
+      return 400;
+    }
   }
-}
-
 
   Future<List<Symptom>> getAllSymptoms() async {
     try {
@@ -64,9 +73,11 @@ class FirebaseService {
 
       if (snapshot.value != null) {
         Map<dynamic, dynamic> data = snapshot.value as Map<dynamic, dynamic>;
-        
+
         data.forEach((key, value) {
-          var symptom = Symptom(name: value['name']/*, chiefComplaints: value['chiefComplaints']*/);
+          var symptom = Symptom(
+              name: value[
+                  'name'] /*, chiefComplaints: value['chiefComplaints']*/);
           symptomList.add(symptom);
         });
       }
@@ -97,21 +108,18 @@ class FirebaseService {
       return chiefComplaintsList;
     } catch (e) {
       print('Error getting data: $e');
-      return []; 
+      return [];
     }
   }
 
-Future<UserClass?> getUser(String uid) async {
+  Future<UserClass?> getUser(String uid) async {
     try {
       DataSnapshot snapshot = await _usersRef.child(uid).get();
 
       if (snapshot.value != null) {
         Map<dynamic, dynamic> data = snapshot.value as Map<dynamic, dynamic>;
 
-        var user = UserClass(
-          email: data['email'],
-          role: data["role"]
-        );
+        var user = UserClass(email: data['email'], role: data["role"]);
 
         return user;
       }
@@ -133,7 +141,10 @@ Future<UserClass?> getUser(String uid) async {
         Map<dynamic, dynamic> data = snapshot.value as Map<dynamic, dynamic>;
 
         data.forEach((key, value) {
-          var user = UserClass(email: value['email'], role: value['role']/*, chiefComplaints: value['chiefComplaints']*/);
+          var user = UserClass(
+              email: value['email'],
+              role: value[
+                  'role'] /*, chiefComplaints: value['chiefComplaints']*/);
           users.add(user);
           // testing instances
           print(user.email);
@@ -143,6 +154,19 @@ Future<UserClass?> getUser(String uid) async {
       return users;
     } catch (e) {
       print('Error getting users: $e');
+      return [];
+    }
+  }
+
+  Future<List<UserClass>> getUsersWithActiveRequests() async {
+    try {
+      List<UserClass> allUsers = await getAllUsers();
+      List<UserClass> usersWithActiveRequests =
+          allUsers.where((user) => user.activeRequest == true).toList();
+
+      return usersWithActiveRequests;
+    } catch (e) {
+      print('Error getting users with active requests: $e');
       return [];
     }
   }
