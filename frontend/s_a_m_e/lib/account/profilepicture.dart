e5350824ca9b0b1eb46043315ae5e8f3f7945198
Connor McPhilliamy <jmcphilliamy3@gatech.edu>
2024-03-08 22:30:24 -0500

    cleaning up profile picture work

--- frontend/s_a_m_e/lib/account/profilepicture.dart
+++ frontend/s_a_m_e/lib/account/profilepicture.dart
@@ -2,15 +2,12 @@
 // above used to get rid of blue underline for
 // wanting const, even though would throw error since stateful
 
-import 'dart:io';
 import 'dart:typed_data';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:s_a_m_e/firebase/firebase_service.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:http/http.dart' as http;
 
 class ProfilePicturePage extends StatefulWidget {
   const ProfilePicturePage({super.key});
--- frontend/s_a_m_e/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/s_a_m_e/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import firebase_auth
 import firebase_core
 import firebase_database
 import firebase_storage
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -20,5 +19,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
